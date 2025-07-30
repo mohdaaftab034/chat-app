30994ebcf410481582008b890be1acdfb8e22372
Mohd Aaftab <aaftabansari034@gmail.com>
2025-07-30 13:42:39 +0530

    change

--- server/server.js
+++ server/server.js
@@ -43,6 +43,10 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("server is running on port: " + PORT))
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> console.log("server is running on port: " + PORT))
+}
+//Export server for vercel
+export default server;
 
